5a59312bedb938be55d91a0051200f1e4bc9a6e3
Evgeniy Spitsyn <evgeniy@spitsyn.net>
2011-12-12 01:58:48 +0300

    Full Auto v.0.2[beta]

--- AutoGanjaFarm.js
+++ AutoGanjaFarm.js
@@ -8,19 +8,28 @@
 // @license        GPL v3
 // ==/UserScript==
 
-//TODO:Автопосадка
 var a=document.getElementsByTagName("body")[0].innerHTML;
 var t=10;
-
-if(a.match(/уже пора/i)!=null||a.match(/Земля не обработана/i)!=null){
-	searchUrl(document.links);
+var req=true;//Запрос автопосадки (true), Автопосадка (false)
+var r=false;
+if(a.match(/Грядка пустая./i)!=null){
+	if(req!=false)r=confirm("Посадить?")
+	if(r==true||req==false){
+		document.forms[1].submit();
+	}else{
+		alert("Вы отказались от посадки! Выберите вручную!")
+	}
 }else{
-	if(a.match(/через ([0-9]*) мин/i)!=null){
-		t=Math.floor(Math.random()*a.match(/через ([0-9]*) мин/i)[1]);
-		i=self.setInterval("location.reload();",((Math.floor(Math.random()*t))*60000));
+	if(a.match(/уже пора/i)!=null||a.match(/Земля не обработана/i)!=null){
+		searchUrl(document.links);
+	}else{
+		if(a.match(/через ([0-9]*) мин/i)!=null){
+			t=Math.floor(Math.random()*a.match(/через ([0-9]*) мин/i)[1])+Math.floor(Math.random()*3);
+			//alert(t);
+			i=self.setInterval("location.reload();",(((t==0)?1:t)*60000));
+		}
 	}
 }
-
 function searchUrl(arr){
     for(var i=0;i<arr.length;i++){
         if((arr[i].href.indexOf("action=cultivate")>-1
