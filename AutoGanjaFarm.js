9bcdb83115f58995a0734ef15e006ad4c807c3b8
Evgeniy Spitsyn <evgeniy@spitsyn.net>
2013-06-30 23:04:52 +0300

    rename repo, fix update urls, fix markdown

--- AutoGanjaFarm.js
+++ AutoGanjaFarm.js
@@ -2,8 +2,8 @@
 // @id             AutoGanjaFarm@dexif
 // @name           AutoGanjaFarm
 // @namespace      ganjawars.ru
-// @updateURL      https://github.com/dexif/AutoGanjaFerm/raw/master/AutoGanjaFarm.meta.js
-// @downloadURL    https://github.com/dexif/AutoGanjaFerm/raw/master/AutoGanjaFarm.js
+// @updateURL      https://github.com/dexif/AutoGanjaFarm/raw/master/AutoGanjaFarm.meta.js
+// @downloadURL    https://github.com/dexif/AutoGanjaFarm/raw/master/AutoGanjaFarm.js
 // @include        http://www.ganjawars.ru/ferma.php*
 // @include        http://ganjawars.ru/ferma.php*
 // @match          http://www.ganjawars.ru/ferma.php*
