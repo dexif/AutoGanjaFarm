955d2ec8a234c8f8ddf97e6c816fa0da1cddde5d
Evgeniy Spitsyn <evgeniy@spitsyn.net>
2013-06-30 18:23:24 +0300

    fix js and readme formatting, added meta file

--- AutoGanjaFarm.js
+++ AutoGanjaFarm.js
@@ -1,72 +1,81 @@
 // ==UserScript==
+// @id             AutoGanjaFarm@dexif
 // @name           AutoGanjaFarm
 // @namespace      ganjawars.ru
+// @updateURL      https://github.com/dexif/AutoGanjaFerm/raw/master/AutoGanjaFarm.meta.js
+// @downloadURL    https://github.com/dexif/AutoGanjaFerm/raw/master/AutoGanjaFarm.js
 // @include        http://www.ganjawars.ru/ferma.php*
 // @include        http://ganjawars.ru/ferma.php*
-// @version        0.2 [beta]
+// @match          http://www.ganjawars.ru/ferma.php*
+// @match          http://ganjawars.ru/ferma.php*
+// @grant          none
+// @version        0.3 [beta]
 // @author         Evgeniy [Dexif] Spitsyn (http://Spitsyn.net)
 // @license        GPL v3
 // ==/UserScript==
 //TODO: карта посадки
 //SETTINGS
-var req=false;//Запрос автопосадки (true), Автопосадка (false)
-var r=false;//Стандартный выбор если без запроса автопосадки
-var pageid=1;//Страница на которой ноходится растение 1-10
-var plantid=3;//Номер растения на странице 1-4
+var req = false; //Запрос автопосадки (true), Автопосадка (false)
+var r = false; //Стандартный выбор если без запроса автопосадки
+var pageid = 1; //Страница на которой ноходится растение 1-10
+var plantid = 3; //Номер растения на странице 1-4
 //SETTINGS
 
-var a=document.getElementsByTagName("body")[0].innerHTML;
-var t=1;
-pageid=(pageid<1)?0:(pageid-1);
-plantid=(plantid<1)?0:(plantid-1);
-if(a.match(/Грядка пустая./i)!=null){
-	if(req!=false)r=confirm("Посадить повторно?")
-	if(r==true||req==false){
-		if(location.href.indexOf("page_id="+pageid)<0){
-				searchPageUrl(document.links);
-		}else{
-			document.getElementsByName("plant_id")[plantid].checked=true;
+var a = document.getElementsByTagName("body")[0].innerHTML;
+var t = 1;
+pageid = (pageid < 1) ? 0 : (pageid - 1);
+plantid = (plantid < 1) ? 0 : (plantid - 1);
+if (a.match(/Грядка пустая./i) != null) {
+	if (req != false) r = confirm("Посадить повторно?")
+	if (r == true || req == false) {
+		if (location.href.indexOf("page_id=" + pageid) < 0) {
+			searchPageUrl(document.links);
+		} else {
+			document.getElementsByName("plant_id")[plantid].checked = true;
 			document.forms[1].submit();
 		}
-	}else{
+	} else {
 		alert("Вы отказались от посадки! Выберите вручную!");
 		checktimer();
 	}
-}else{
+} else {
 	checktimer();
 
 }
-function searchUrl(arr){
-    for(var i=0;i<arr.length;i++){
-        if((arr[i].href.indexOf("action=cultivate")>-1
-			||arr[i].href.indexOf("action=extract")>-1
-			||arr[i].href.indexOf("action=water")>-1
-			||arr[i].innerHTML.indexOf("собрать")>-1
-			||arr[i].innerHTML.indexOf("полить")>-1)&&arr[i].href.indexOf("#")==-1){
+
+function searchUrl(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if ((arr[i].href.indexOf("action=cultivate") > -1
+			|| arr[i].href.indexOf("action=extract") > -1
+			|| arr[i].href.indexOf("action=water") > -1
+			|| arr[i].innerHTML.indexOf("собрать") > -1
+			|| arr[i].innerHTML.indexOf("полить") > -1) && arr[i].href.indexOf("#") == -1) {
 			window.location.assign(arr[i].href);
 		}
-    }
+	}
 }
-function searchPageUrl(arr){
-    for(var i=0;i<arr.length;i++){
-        if((arr[i].href.indexOf("page_id="+pageid)>-1)&&arr[i].href.indexOf("#")==-1){
+
+function searchPageUrl(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if ((arr[i].href.indexOf("page_id=" + pageid) > -1) && arr[i].href.indexOf("#") == -1) {
 			//window.location.assign(arr[i].href);
-			i=self.setInterval("window.location.assign('"+arr[i].href+"');",(Math.floor(Math.random()*3000)));
-			//alert("serchUrl:"+arr[i].href);
+			i = self.setInterval("window.location.assign('" + arr[i].href + "');", (Math.floor(Math.random() * 3000)));
+			//console.log("searchUrl:"+arr[i].href);
 			return true;
 		}
-    }
+	}
 	return false;
-	//alert("serchUrl:null");
+	//console.log("searchUrl:null");
 }
-function checktimer(){
-	if(a.match(/уже пора/i)!=null||a.match(/Земля не обработана/i)!=null){
-			searchUrl(document.links);
-	}else{
-		if(a.match(/через ([0-9]*) мин/i)!=null){
-			t=(Math.floor(Math.random()*a.match(/через ([0-9]*) мин/i)[1]))+(Math.floor(Math.random()*3));
-			i=self.setInterval("location.reload();",(((t==0)?1:t)*60000));
-			//alert("checktimer:"+t);
+
+function checktimer() {
+	if (a.match(/уже пора/i) != null || a.match(/Земля не обработана/i) != null) {
+		searchUrl(document.links);
+	} else {
+		if (a.match(/через ([0-9]*) мин/i) != null) {
+			t = (Math.floor(Math.random() * a.match(/через ([0-9]*) мин/i)[1])) + (Math.floor(Math.random() * 3));
+			i = self.setInterval("location.reload();", (((t == 0) ? 1 : t) * 60000));
+			//console.log("checktimer:"+t);
 		}
 	}
 }
---
+++ AutoGanjaFarm.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             AutoGanjaFarm@dexif
+// @name           AutoGanjaFarm
+// @namespace      ganjawars.ru
+// @updateURL      https://github.com/dexif/AutoGanjaFerm/raw/master/AutoGanjaFarm.meta.js
+// @downloadURL    https://github.com/dexif/AutoGanjaFerm/raw/master/AutoGanjaFarm.js
+// @include        http://www.ganjawars.ru/ferma.php*
+// @include        http://ganjawars.ru/ferma.php*
+// @match          http://www.ganjawars.ru/ferma.php*
+// @match          http://ganjawars.ru/ferma.php*
+// @grant          none
+// @version        0.3 [beta]
+// @author         Evgeniy [Dexif] Spitsyn (http://Spitsyn.net)
+// @license        GPL v3
+// ==/UserScript==
